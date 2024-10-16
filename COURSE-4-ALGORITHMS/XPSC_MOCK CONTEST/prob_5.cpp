8d106c88c947d1f7eb4577726254529201a5bbc4
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-10-16 15:23:28 +0600

    16 october 2nd commit

--- COURSE-4-ALGORITHMS/XPSC_MOCK CONTEST/prob_5.cpp
+++ COURSE-4-ALGORITHMS/XPSC_MOCK CONTEST/prob_5.cpp
@@ -1,3 +1,5 @@
+
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -65,6 +67,129 @@ int main()
     }
 
     cout << res << endl;
+
+    return 0;
+}
+*/
+
+// 5th Solution: Mina and Moni
+
+/*
+int findMinDifference(vector<int> &coins, int N)
+{
+    int totalSum = 0;
+    for (int coin : coins)
+        totalSum += coin;
+
+    int targetSize = N / 2;     // Target size for one of the subsets
+    int halfSum = totalSum / 2; // We want to approach half of the total sum
+
+    // DP table to check possible sums with i coins
+    // dp[i][j] will be true if we can achieve sum j using exactly i coins
+    vector<vector<bool>> dp(targetSize + 1, vector<bool>(halfSum + 1, false));
+    dp[0][0] = true; // 0 coins make a sum of 0
+
+    // Dynamic programming approach to fill the dp table
+    for (int coin : coins)
+    {
+        for (int i = targetSize; i > 0; --i)
+        {
+            for (int j = halfSum; j >= coin; --j)
+            {
+                if (dp[i - 1][j - coin])
+                {
+                    dp[i][j] = true;
+                }
+            }
+        }
+    }
+
+    // Now we find the maximum sum that can be achieved with ...
+    // exactly targetSize coins
+    int bestSum = 0;
+    for (int j = 0; j <= halfSum; ++j)
+    {
+        if (dp[targetSize][j])
+        {
+            bestSum = j;
+        }
+    }
+
+    // The other subset sum will be totalSum - bestSum
+    int otherSum = totalSum - bestSum;
+    return abs(bestSum - otherSum);
+}
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+int findMinDifference(vector<int> &coins, int N)
+{
+    int totalSum = 0;
+    for (int coin : coins)
+        totalSum += coin;
+
+    // Half size constraint
+    int halfSum = totalSum / 2;
+    int halfSize = N / 2;
+
+    // DP table where dp[j] indicates whether it's possible to form sum j
+    // without caring about the number of coins used yet
+    vector<vector<bool>> dp(N + 1, vector<bool>(totalSum + 1, false));
+
+    dp[0][0] = true; // We can always make sum 0 with 0 coins
+
+    // Fill the dp table
+    for (int coin : coins)
+    {
+        // Traverse in reverse to ensure each coin is only considered once
+        for (int i = N; i >= 1; --i)
+        {
+            for (int j = totalSum; j >= coin; --j)
+            {
+                dp[i][j] = dp[i][j] || dp[i - 1][j - coin];
+            }
+        }
+    }
+
+    // We need to split into two groups: halfSize coins for each sister
+    int bestSum = 0;
+    for (int j = 0; j <= halfSum; j++)
+    {
+        if (dp[halfSize][j])
+        { // Only consider subsets of size halfSize
+            bestSum = j;
+        }
+    }
+
+    // The other subset sum will be totalSum - bestSum
+    int otherSum = totalSum - bestSum;
+    return abs(bestSum - otherSum);
+}
+
+int main()
+{
+    int T;
+    cin >> T; // Number of test cases
+
+    while (T--)
+    {
+        int N;
+        cin >> N; // Number of coins
+
+        vector<int> coins(N);
+        for (int i = 0; i < N; ++i)
+        {
+            cin >> coins[i];
+        }
+
+        // Find and print the minimum difference for the current test case
+        cout << findMinDifference(coins, N) << endl;
+    }
 
     return 0;
 }
